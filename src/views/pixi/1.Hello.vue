dc0f97a69a87e5e0133b9f45df165daff014a01b
lcc19941214 <245987239@qq.com>
2020-05-09 14:26:44 +0800

    feat: support change bgColor in 1.hello

--- src/views/pixi/1.Hello.vue
+++ src/views/pixi/1.Hello.vue
@@ -1,15 +1,39 @@
 <template>
-  <div id="hello"></div>
+  <div id="hello">
+    <div class="main"></div>
+
+    <div>
+      <div>
+        <label for>backgroundColor:&nbsp;</label>
+        <input type="text" placeholder="#000000" value="bgColor" v-model="bgColor" />
+        &nbsp;&nbsp;
+        <button @click="updateRendererBgColor">update</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import * as PIXI from 'pixi.js';
 
+const app = new PIXI.Application({ width: 300, height: 300 });
+
 export default {
-  data: () => ({}),
+  name: 'Hello',
+  data: () => {
+    return {
+      app,
+      bgColor: app.renderer._backgroundColorString
+    };
+  },
+  methods: {
+    updateRendererBgColor() {
+      app.renderer.backgroundColor = this.bgColor.replace(/^#/, '0x');
+    }
+  },
   mounted() {
-    const app = new PIXI.Application({ width: 300, height: 300 });
-    document.getElementById('hello').appendChild(app);
+    const root = document.getElementById('hello');
+    root.querySelector('.main').appendChild(app.view);
   }
 };
 </script>
